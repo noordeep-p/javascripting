aca0c8d370998937bb6d64813b196aca76625f96
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-06-29 02:16:20 -0400

    intial save of javascripting directory

---
+++ if-statement.js
@@ -0,0 +1,8 @@
+let fruit = 'orange';
+let n = fruit.length;
+
+if(n > 4) {
+console.log('The fruit name has more than five characters.');
+} else {
+console.log('The fruit name has five characters or less.')
+};
